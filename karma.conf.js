2dbb29af34bd68927fc3f386fbe15946e6b53b93
Marcus Giannamore <marcus.giannamore@dcsg.com>
2018-09-14 16:18:59 -0400

    added my own karma testing

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    files: ['test/**/*.js'],
+    reporters: ['progress'],
+    port: 9876,  // karma web server port
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['ChromeHeadless'],
+    autoWatch: false,
+    // singleRun: false, // Karma captures browsers, runs the tests and exits
+    concurrency: Infinity
+  })
+}
